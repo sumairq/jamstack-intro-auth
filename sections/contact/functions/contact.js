92be99010a183e61d463bf86178fd66b7a609d46
Sumair <sumairqaisar992@gmail.com>
2020-11-07 14:25:49 +0500

    feat(contact): Just added the mailgun

--- sections/contact/functions/contact.js
+++ sections/contact/functions/contact.js
@@ -1,9 +1,25 @@
+require('dotenv').config();
+
 exports.handler = (event, _context, callback) => {
-console.log({event});
+  const mailgun = require('mailgun-js');
+  const mg = mailgun({
+    apiKey: process.env.MAILGUN_API_KEY,
+    domain: process.env.MAILGUN_DOMAIN
+  });
 
-callback(null, {
-    statusCode: 200,
-    body: JSON.stringify({boop: true})
-})
-}
+  const data = JSON.parse(event.body);
 
+  const email = {
+    from: 'Jason Lengstorf <jason@lengstorf.com>',
+    to: `${data.name} <${data.email}>`,
+    subject: data.subject,
+    text: data.body
+  };
+
+  mg.messages().send(email, (error, response) => {
+    callback(error, {
+      statusCode: 200,
+      body: JSON.stringify(response)
+    });
+  });
+};
